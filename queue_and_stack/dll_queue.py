01f62a5e2802a8b8942f12380eb8ea962aafa469
Brand <blubrandi0@gmail.com>
2020-04-20 21:08:19 -0600

    doubly_linked_list tests passing

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -8,12 +8,19 @@ class Queue:
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size = self.storage.length
 
     def dequeue(self):
-        pass
+        if self.size < 1:
+            return None
+        else:
+            self.size -= 1
+        return self.storage.remove_from_head()
 
+        
     def len(self):
-        pass
+        return self.size
